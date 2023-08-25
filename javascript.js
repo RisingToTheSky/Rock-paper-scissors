65b425a17d7b6fdc245eb5e9c412619b4ccf0bda
Joshua <141631716+RisingToTheSky@users.noreply.github.com>
2023-08-25 13:33:54 +0200

    Add function
    The function checks if player of computer wins.

--- javascript.js
+++ javascript.js
@@ -4,10 +4,31 @@ function getComputerChoice(choice){
     choice = Math.floor(Math.random() * 3) + 1;
     /* if math.random() has one, return, rock, two, paper, three, scissors*/
     if (choice === 1){
-        return "Rock";
+        return "rock";
     }else if (choice === 2)
-        return "Paper";
+        return "paper";
     else if (choice === 3)
-        return "Scissors"; 
+        return "scissors"; 
 }
+/*playRound function*/
 
+function playRound(playerSelection, computerSelection){
+    if (
+    (computerSelection == "rock" && playerSelection == "scissors") ||
+    (computerSelection == "scissors" && playerSelection == "paper") ||
+    (computerSelection == "paper" && playerSelection == "rock")){
+        return `You lose! ${computerSelection} beats ${playerSelection}`;
+    }else if (
+    (computerSelection == "rock" && playerSelection == "paper") ||
+    (computerSelection == "scissors" && playerSelection == "rock") ||
+    (computerSelection == "paper" && playerSelection == "scissors")){
+        return `You win! ${playerSelection} beats ${computerSelection}`;
+    }else{
+        return `Tie! ${playerSelection} cannot beat ${computerSelection}`;
+    }
+}
+const playerSelection = prompt("Rock paper or scissors?").toLowerCase();
+const computerSelection = getComputerChoice()
+console.log(playerSelection);
+console.log(computerSelection);
+console.log(playRound(playerSelection, computerSelection));
