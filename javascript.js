a23893d4258ab8341bfb99671475db3abab5b8ed
Joshua <141631716+RisingToTheSky@users.noreply.github.com>
2023-08-25 14:39:45 +0200

    Remove some comments

--- javascript.js
+++ javascript.js
@@ -1,10 +1,9 @@
 let computerScore = 0;
 let playerScore = 0;
-/*Computer choice between rock, paper and scissors*/
+
 function getComputerChoice(choice){    
-    /*Use math.random() to choose between rock, paper and scissors*/
     choice = Math.floor(Math.random() * 3) + 1;
-    /* if math.random() has one, return, rock, two, paper, three, scissors*/
+
     if (choice === 1){
         return "rock";
     }else if (choice === 2)
@@ -12,11 +11,8 @@ function getComputerChoice(choice){
     else if (choice === 3)
         return "scissors"; 
 }
-/*Game function*/
 function game(result){
-/*playRound function*/
     function playRound(playerSelection, computerSelection){
-        /*if statement to check who wins*/
         if (
         (computerSelection == "rock" && playerSelection == "scissors") ||
         (computerSelection == "scissors" && playerSelection == "paper") ||
@@ -44,7 +40,7 @@ function game(result){
         console.log(`Computer score: ${computerScore}`);
         console.log(`Round count: ${roundCount}`);
     }
-    /*Capitalize text*/
+    /*Capitalize text for the return functions*/
     function capitalize(string) {
         return string.charAt(0).toUpperCase() + string.slice(1);
     }
