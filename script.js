2bd17049e56c9bbe832576db727485325ad91ea6
Joshua <141631716+RisingToTheSky@users.noreply.github.com>
2023-08-28 15:43:16 +0200

    Remove console.log statements
    Replace with h1 elements with text content.

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
-let computerScore = 0;
-let playerScore = 0;
-
 let playerSelection;
 const buttons = document.querySelectorAll('button');
+const playerText = document.querySelector('#playerText');
+const computerText = document.querySelector('#computerText');
+const resultText = document.querySelector('#resultText');
+
+
 buttons.forEach(button => button.addEventListener("click", () =>{
     playerSelection = button.textContent.toLowerCase();
     computerSelection = getComputerChoice();
     playRound(playerSelection, computerSelection);
-    console.log(playerSelection);
+    playerText.textContent = `Player: ${playerSelection}`;
+    computerText.textContent = `Computer: ${computerSelection}`;
+    resultText.textContent = `Result: ${playRound(playerSelection, computerSelection)}`;
 }))
 
 function getComputerChoice(choice){    
@@ -20,23 +24,23 @@ function getComputerChoice(choice){
     else if (choice === 3)
         return "scissors"; 
 }
+
 function playRound(playerSelection, computerSelection){
     if (
     (computerSelection == "rock" && playerSelection == "scissors") ||
     (computerSelection == "scissors" && playerSelection == "paper") ||
     (computerSelection == "paper" && playerSelection == "rock")){
-        computerScore++;
         return `You lose! ${capitalize(computerSelection)} beats ${capitalize(playerSelection)}`;
     }else if (
     (computerSelection == "rock" && playerSelection == "paper") ||
     (computerSelection == "scissors" && playerSelection == "rock") ||
     (computerSelection == "paper" && playerSelection == "scissors")){
-        playerScore++;
         return `You win! ${capitalize(playerSelection)} beats ${capitalize(computerSelection)}`;
     }else{
         return `Tie! ${capitalize(playerSelection)} cannot beat ${capitalize(computerSelection)}`;
     }
 }
+
     /*Create loop to loop over function and to display round count*/
     /*Capitalize text for the return functions*/
 function capitalize(string) {
